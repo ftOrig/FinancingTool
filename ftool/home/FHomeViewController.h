0fa8af1b38fa38036cdfc90921e9495c7bb4f80d
周利强 <zhoulq@eptok.com>
2018-06-23 11:13:32 +0800

    缺省第三方
    SDWebImage

--- ftool/home/FHomeViewController.h
+++ ftool/home/FHomeViewController.h
@@ -6,8 +6,8 @@
 //  Copyright © 2018年 apple. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "AJContainTableController.h"
 
-@interface FHomeViewController : UIViewController
+@interface FHomeViewController : AJContainTableController
 
 @end
